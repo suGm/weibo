026e0967d1c9b3c52aeade28be9c9539d450590e
sgm <630059110@qq.com>
2019-07-24 03:10:07 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');//指定别名,使其可以在blade模板中使用{{ route('help') }}调用
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+Route::get('signup', 'UsersController@create')->name('signup');//有无/都无差别,laravel兼容两种写法
+
+
