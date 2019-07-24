6b78e9a032f0e415feadca5e7c4b4a1706f7baa4
sgm <630059110@qq.com>
2019-07-24 06:08:08 +0000

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -10,9 +10,10 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'users';//对应要交互的用户数据库
     /**
      * The attributes that are mass assignable.
-     *
+     * 对过滤用户提交字段,只有包含在该属性的字段才能被正常更新
      * @var array
      */
     protected $fillable = [
@@ -21,7 +22,7 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for arrays.
-     *
+     * 当我们要对用户密码或其他敏感信息在用户实例通过数组或JSON显示时进行隐藏
      * @var array
      */
     protected $hidden = [
