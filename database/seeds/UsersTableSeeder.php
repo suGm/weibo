5af9cd6322e265319e9802ab30ad6885593bdc37
suGm <630059110@qq.com>
2019-08-21 12:22:33 +0000

    管理员删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'sugm';
         $user->email = '630059110@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
