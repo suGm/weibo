afc97b4f75077998cdab588958f5572380fbbdaa
suGm <630059110@qq.com>
2019-08-22 09:45:29 +0000

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::resource('users', 'UsersController');//资源路由
 	Route::post('/users', 'UsersController@store')->name('users.store');//创建用户
 	Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');//编辑用户个人资料
 	Route::patch('/users/{user}', 'UsersController@update')->name('users.update');//更新用户
-	Route::delete('/users/{user}', 'UsersController@destory')->name('users.destory');//删除用户
+	Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');//删除用户
 */
 
 Route::get('login', 'SessionsController@create')->name('login');
@@ -41,3 +41,5 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');//邮箱发送重设链接
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');//密码更新页面
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');//执行密码更新操作
+
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);//处理微博创建和删除的请求
