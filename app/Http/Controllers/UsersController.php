8fa555ec47f4b537bd012a534ceb4a44d33343c9
suGm <630059110@qq.com>
2019-08-22 07:16:35 +0000

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -134,13 +134,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '630059110@qq.com';
-        $name = 'sugm';
-        $to = $user->email;
+        $to = '447866679@qq.com';
         $subject = '感谢注册 Weibo 应用!请确认你的邮箱。';
 
-        Mail::send($view, $data, function($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 }
