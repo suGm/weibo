ac4c1cd65c3d1feb96dc7c5d541777c2fd112ac6
suGm <630059110@qq.com>
2019-08-22 06:55:45 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,9 @@ Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 //注册验证email
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+//重置密码
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');//显示重置密码的邮箱发送页面
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');//邮箱发送重设链接
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');//密码更新页面
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');//执行密码更新操作
